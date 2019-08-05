e91c162593354f84016e1d009d9c0ce622a4ed69
gaoxuan <gaoxuan44@gmail.com>
2019-08-05 23:01:27 +0800

    add splash activity

---
+++ app/src/main/java/com/gx/fileexplorer/activity/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.gx.fileexplorer.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+            }
+        }).start();
+    }
+}
